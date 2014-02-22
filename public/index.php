acf522ec2b93211e76662c173e1e7f206d66b210
cjds <cjds@live.com>
2014-02-22 18:27:54 +0530

    change in index.php

--- public/index.php
+++ public/index.php
@@ -18,7 +18,7 @@
 |
 */
 
-require $dir.'/../bootstrap/autoload.php';
+require __DIR__.'/../bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -32,7 +32,7 @@ require $dir.'/../bootstrap/autoload.php';
 |
 */
 
-$app = require_once $dir.'/../bootstrap/start.php';
+$app = require_once __DIR__.'/../bootstrap/start.php';
 
 /*
 |--------------------------------------------------------------------------
