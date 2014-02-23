e5ba087cd5c48ab83a5f6545c622294a347d7433
cjds <cjds@live.com>
2014-02-23 19:41:19 +0530

    changes to register

--- app/views/register.blade.php
+++ app/views/register.blade.php
@@ -2,24 +2,23 @@
 
 @section('content')
 <div class="row">
-<div class="span4 offset1">
-	<div class="well">
-		<legend>Please Register</legend>
+<div class="large-4 small-12 medium-6 columns">
+		<h3>Please Register</h3>
 		{{Form::open(array('url'=>'register'))}}
 		@if($errors->any())
-			<div class="alert alert-error">
-				<a href="#" class="close" data-dismiss="alert">&times;</a>
-				{{implode('',$errors->all('<li class="error">:message</li>'))}}
+			<div class='errors'>
+				
+				{{implode('',$errors->all('<div data-alert class="alert-box warning">:message <a href="#" class="close">&times;</a></div>'))}}
 			</div>
 		@endif
 		{{Form::text('user_username','',array('placeholder'=>'Enter Username'))}}<br>
 		{{Form::text('user_email','',array('placeholder'=>'Enter Email Address'))}}<br>
 		{{Form::password('user_password', array('placeholder'=>'Enter Password'));}}<br>
 		{{Form::password('user_confpassword', array('placeholder'=>'Confirm Password'));}}<br>
-		{{Form::submit('Register', array('class'=>'btn btn-success'));}}
-		{{HTML::link('login', 'Login', array('class'=>'btn btn-primary'));}}
+		{{Form::submit('Register', array('class'=>'button success'));}}
+		{{HTML::link('login', 'Login', array('class'=>'button '));}}
 		{{Form::close();}}
-	</div>
+
 </div>
 </div>
 
